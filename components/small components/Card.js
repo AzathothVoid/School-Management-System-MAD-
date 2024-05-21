384288853972ed9395023672482c138a15b56b8a
Muhammad Salahuddin <salahuddin7muhammad@gmail.com>
2024-05-22 00:59:35 +0500

    testing again again

---
+++ components/small components/Card.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Card() {
+  return (
+    <View>Cardddd</View>
+  )
+}
+
+export default Card
--- components/small components/PrimaryButton.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function PrimaryButton() {
-  return (
-    <View>Button</View>
-  )
-}
-
-export default PrimaryButton
