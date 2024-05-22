d75df439c3e650690220ea6025221e398236e4ed
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2024-05-23 03:01:43 +0500

    Resolving all merge conflicts

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SafeAreaView, View, StyleSheet, Text} from 'react-native';
+import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
 import {GluestackUIProvider} from '@gluestack-ui/themed';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -17,25 +17,33 @@ import ViewSyllabus from './screens/student/ViewSyllabus';
 import ViewFees from './screens/student/ViewFees';
 import ViewMarks from './screens/student/ViewMarks';
 
-
 function App() {
-
   //pass to ViewSyllabus
-  var subjects = ["English", "Urdu", "Math", "General Knowledge", "Social Studies", "Islamiyat", "Computer Part 1", "Computer Part 2", "Quran"]
+  var subjects = [
+    'English',
+    'Urdu',
+    'Math',
+    'General Knowledge',
+    'Social Studies',
+    'Islamiyat',
+    'Computer Part 1',
+    'Computer Part 2',
+    'Quran',
+  ];
 
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
       <GluestackUIProvider config={config}>
-        <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Navigator initialRouteName="ViewFees">
           <Stack.Screen name="Welcome" component={Welcome} />
-          <Stack.Screen name="Admin" component={Admin} />
+          {/* <Stack.Screen name="Admin" component={Admin} />
           <Stack.Screen name="Student" component={Student} />
-          <Stack.Screen name="Teacher" component={Teacher} />
-          <Stack.Screen name='ViewFees' component={ViewFees} />
-            <Stack.Screen name='ViewSyllabus' component={ViewSyllabus} />
-            <Stack.Screen name='ViewMarks' component={ViewMarks} />
+          <Stack.Screen name="Teacher" component={Teacher} /> */}
+          <Stack.Screen name="ViewFees" component={ViewFees} />
+          <Stack.Screen name="ViewSyllabus" component={ViewSyllabus} />
+          <Stack.Screen name="ViewMarks" component={ViewMarks} />
         </Stack.Navigator>
       </GluestackUIProvider>
     </NavigationContainer>
