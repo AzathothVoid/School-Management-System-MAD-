407ce72bdcb267438d1429c9e8c94898894e847f
AzathothVoid <90100139+AzathothVoid@users.noreply.github.com>
2024-05-23 00:16:45 +0500

    Setting up react-native-navigation

--- App.js
+++ App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import {GluestackUIProvider, Text} from '@gluestack-ui/themed';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {config} from '@gluestack-ui/config';
+
+//importing screens
+import Welcome from './Screens/Welcome';
+import Admin from './Screens/Admin';
+import Student from './Screens/Student';
+import Teacher from './Screens/Teacher';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    <SafeAreaView style={styles.backgroundStyle}>
-      <Text>Welcome</Text>
-    </SafeAreaView>
+    <NavigationContainer>
+      <GluestackUIProvider config={config}>
+        <Stack.Navigator initialRouteName="Welcome">
+          <Stack.Screen name="Welcome" component={<Welcome />} />
+          <Stack.Screen name="Admin" component={<Admin />} />
+          <Stack.Screen name="Student" component={<Student />} />
+          <Stack.Screen name="Teacher" component={<Teacher />} />
+        </Stack.Navigator>
+      </GluestackUIProvider>
+    </NavigationContainer>
   );
 }
 
